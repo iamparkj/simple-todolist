de2f8e01513c4acd9602a6196792c333616c8175
Park Junyoung <bloomwayz@snu.ac.kr>
2025-09-06 11:40:00 +0900

    :tada: Initial commit

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import './style.css'
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+    <h1>Vite + TypeScript</h1>
+    <div class="card">
+      <button id="counter" type="button">Hello!</button>
+    </div>
+  </div>
+`
